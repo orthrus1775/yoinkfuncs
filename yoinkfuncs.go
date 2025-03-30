46e462e6bf7a7bdd6b7b4e83614afbcd57435fa0
admin <none@null.void>
2025-03-30 16:24:04 -0400

    impl RequestNewFileInfoForm func

--- yoinkfuncs.go
+++ yoinkfuncs.go
@@ -31,4 +31,88 @@ func addPrefixToFileName(fp string) string {
 	return filepath.Join(dir, newBaseName)
 }
 
+func RequestNewFileInfoForm(fvi FVInfo) FVInfo {
 
+		form := huh.NewForm(
+			// huh.NewGroup(huh.NewNote().
+			// 	Title("File Version Info").
+			// 	Description("Customize the File Version Info.\n\n").
+			// 	Next(true).
+			// 	NextLabel("Next"),
+			// ),
+	
+			// huh.NewGroup(huh.NewNote().
+			// 	Title("File Info").
+			// 	Description("Input New File Info.\n\n"),
+			// ),
+	
+			huh.NewGroup(
+				huh.NewInput().
+				Title("File Info").
+				Description("Input New File Info.\n\n").
+	
+				Value(&fvi.CompanyName).
+				Description("CompanyName").
+				Placeholder(fvi.CompanyName),
+	
+				huh.NewInput().
+				Value(&fvi.FileDescription).
+				Description("FileDescription").
+				Placeholder(fvi.FileDescription),
+	
+				huh.NewInput().
+				Value(&fvi.FileVersion).
+				Description("FileVersion").
+				Placeholder(fvi.FileVersion),
+	
+				huh.NewInput().
+				Value(&fvi.InternalName).
+				Description("InternalName").
+				Placeholder(fvi.InternalName),
+	
+				huh.NewInput().
+				Value(&fvi.ProductName).
+				Description("ProductName").
+				Placeholder(fvi.ProductName),
+	
+				huh.NewInput().
+				Value(&fvi.ProductVersion).
+				Description("ProductVersion").
+				Placeholder(fvi.ProductVersion),
+	
+				huh.NewInput().
+				Value(&fvi.OriginalFilename).
+				Description("OriginalFilename").
+				Placeholder(fvi.OriginalFilename),
+	
+			),
+	
+			huh.NewGroup(
+				huh.NewInput().
+				Value(&fvi.Copyright).
+				Description("Copyright").
+				Placeholder(fvi.Copyright),
+	
+				huh.NewInput().
+				Value(&fvi.Trademark).
+				Description("Trademark").
+				Placeholder(fvi.Trademark),
+	
+				huh.NewInput().
+				Value(&fvi.Language).
+				Description("Language").
+				Placeholder(fvi.Language),
+	
+				huh.NewInput().
+				Value(&fvi.Comments).
+				Description("Comments").
+				Placeholder(fvi.Comments),
+			),
+	
+		).WithLayout(huh.LayoutStack)
+
+		form.Run()
+
+		return fvi
+
+}
