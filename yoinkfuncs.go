a74993076e679121e5d082af3cce4030ea9d0dfb
admin <none@null.void>
2025-03-30 16:31:51 -0400

    git wonk

--- yoinkfuncs.go
+++ yoinkfuncs.go
@@ -1,7 +1,21 @@
 package yoinkfuncs
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
 	"path/filepath"
+	"reflect"
+	"strings"
+	"time"
+	"unsafe"
+
+	"github.com/charmbracelet/huh"
+	"github.com/fatih/color"
+	"github.com/tc-hib/winres"
+	"github.com/tc-hib/winres/version"
 )
 
 // Required Entries
