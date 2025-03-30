eb6e1cc9d18a16f9cfcf2566d14fbbd1e1a1634e
admin <none@null.void>
2025-03-30 15:51:58 -0400

    implement FVInfo struct

--- yoinkfuncs.go
+++ yoinkfuncs.go
@@ -18,3 +18,20 @@ import (
 	"github.com/tc-hib/winres/version"
 )
 
+// Required Entries
+// Ref: https://learn.microsoft.com/en-us/windows/win32/menurc/versioninfo-resource
+	type FVInfo struct {
+		CompanyName		string
+		FileDescription    string
+		FileVersion        string
+		InternalName		string
+		ProductName        string
+		ProductVersion     string
+		OriginalFilename   string
+	// Extended Entries
+	// Not Required But Can Be Commonplace
+		Copyright       string
+		Trademark		string
+		Language		string
+		Comments		string
+}
